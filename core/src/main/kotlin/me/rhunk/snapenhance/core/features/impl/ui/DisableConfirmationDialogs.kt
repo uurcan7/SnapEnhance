9195ceea543efec6c262ca64f0647d288caaa35b
rhunk <101876869+rhunk@users.noreply.github.com>
2024-04-20 00:14:36 +0200

    fix: disable confirmation dialogs - duplicate question keys

--- core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/ui/DisableConfirmationDialogs.kt
+++ core/src/main/kotlin/me/rhunk/snapenhance/core/features/impl/ui/DisableConfirmationDialogs.kt
@@ -20,13 +20,15 @@ class DisableConfirmationDialogs : Feature("Disable Confirmation Dialogs", loadP
         val questions = listOf(
             "erase_message" to "erase_learn_more_dialog_title",
             "erase_message" to "erase_dialog_title",
+            "erase_message" to "snap_erase_dialog_title",
+            "erase_message" to "snap_erase_learn_more_dialog_title",
             "remove_friend" to "action_menu_remove_friend_question",
             "block_friend" to "action_menu_block_friend_question",
             "ignore_friend" to "action_menu_ignore_friend_question",
             "hide_friend" to "action_menu_hide_friend_question",
             "hide_conversation" to "hide_or_block_clear_conversation_dialog_title",
             "clear_conversation" to "action_menu_clear_conversation_dialog_title"
-        ).associate { pair ->
+        ).map { pair ->
             pair.first to runCatching {
                 Pattern.compile(
                     context.resources.getString(context.resources.getIdentifier(pair.second, "string"))
